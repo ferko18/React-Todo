7ce5525f7f5229363fbcf570b94458f39ac2db5a
Firealem Erko <firealemg@yahoo.com>
2019-04-24 18:03:17 -0400

    added event listners to form, to be added to appjs

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -1,18 +1,14 @@
-import React from 'react'
-import './Todo.css'
+import React from "react";
+import "./Todo.css";
 
-const TodoForm = function ()
-{
-    return (
-        <div className='TodoForm'>
-           
-           <input placeholder="add a todo list" name='todo'></input>
-            <button>Add Todo</button>   
-            <button>Clear Completed </button> 
-
-        </div>
-    );
-
-}
+const TodoForm = function(props) {
+  return (
+    <div className="TodoForm">
+      <input placeholder="add a task" name="task" type="text" onChange={props.newTask} value ={props.task}/>
+      <button onClick={props.addTodo}>Add Todo</button>
+      <button onClick={props.clearInput}>Clear Completed </button>
+    </div>
+  );
+};
 
 export default TodoForm;
