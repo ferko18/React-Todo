79a15cf7a95353d7e53c5880ddf4fc60e33cf025
Firealem Erko <firealemg@yahoo.com>
2019-04-24 16:07:47 -0400

    added list component

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,18 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from 'react'
+import './Todo.css'
+
+const TodoList = function ()
+{
+    return (
+        <div className='TodoList'>
+           
+           <li>hi</li>
+
+        </div>
+    );
+
+}
+
+export default TodoList;
